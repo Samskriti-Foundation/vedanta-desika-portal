b2a2b0eb8c10073352e09c2e35b04c185d947fad
Pranav K Bhaskar <pkb19032002@gmail.com>
2024-06-10 11:08:17 +0530

    Added horizontal and vertical view

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,9 +1,10 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 home_bp = Blueprint("home", __name__, url_prefix="/")
 
 
 @home_bp.route("/")
 def home():
-    return render_template("pages/home/index.html")
+    view = request.args.get("view", "horizontal")
+    return render_template("pages/home/index.html", view=view)
 
